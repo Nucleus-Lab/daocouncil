b91ce15cc27da59005cb6440f8fc96f38f2c493a
Xiangyu Wong <wongxiangyu22@gmail.com>
2025-02-10 00:22:34 +0800

    Allow all origins

--- backend/main.py
+++ backend/main.py
@@ -50,7 +50,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", os.getenv("FRONTEND_BASE_URL")],
+    allow_origins=["*"],  # Allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
